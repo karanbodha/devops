10c72cf0ccd982a4914112b40a1ce20f4bd5a83f
karanbodha <karanbodha@gmail.com>
2022-04-27 11:04:41 +0530

    updated

---
+++ pythonjenkins.py
@@ -0,0 +1,9 @@
+import sys 
+a = int(sys.argv[1])
+
+b = int(sys.argv[2])
+
+print ("Addition",a+b)
+print ("Subtraction", a-b)
+print ("Multiplication of ", a ,"and", b ,"is", a*b)
+print ("Division of ", a ,"and", b, "is", a/b)
